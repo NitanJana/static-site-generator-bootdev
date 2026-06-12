4686198392a179e20ae6f35f4697c5389ba90ca7
nitan <67586363+NitanJana@users.noreply.github.com>
2026-06-12 15:39:15 +0530

    copy static files

--- src/main.py
+++ src/main.py
@@ -1,8 +1,26 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
+source= "./static"
+destination= "./public"
 
 def main():
-    textnode = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(textnode)
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+
+    copytree(source, destination)
+
+def copytree(source_dir: str, dest_dir: str) -> None :
+    if not os.path.exists(dest_dir):
+        os.mkdir(dest_dir)
+
+    for item in os.listdir(source_dir):
+        from_path = os.path.join(source_dir, item)
+        to_path = os.path.join(dest_dir, item)
+
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, to_path)
+        else:
+            copytree(from_path, to_path)
 
 main()
