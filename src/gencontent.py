c9bb6da734917e95d84c5a269b21d93f3ef83149
nitan <67586363+NitanJana@users.noreply.github.com>
2026-06-13 11:52:29 +0530

    deploy setup

--- src/gencontent.py
+++ src/gencontent.py
@@ -11,7 +11,7 @@ def extract_title(markdown: str) -> str:
             return line[2:]
     return "Untitled"
 
-def generate_page(from_path: str, template_path: str, dest_path: str | Path) -> None:
+def generate_page(from_path: str, template_path: str, dest_path: str | Path, basepath: str) -> None:
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as f:
         contents =  f.read()
@@ -21,17 +21,19 @@ def generate_page(from_path: str, template_path: str, dest_path: str | Path) -> 
     title = extract_title(contents)
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(dest_path, "w") as d:
         d.write(template)
 
-def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str) -> None:
+def generate_pages_recursive(dir_path_content: str, template_path: str, dest_dir_path: str, basepath: str) -> None:
     for item in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
