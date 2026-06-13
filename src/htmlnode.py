f9b3a549635f83d1b99a26f9163cafb67101ba20
nitan <67586363+NitanJana@users.noreply.github.com>
2026-06-13 10:59:04 +0530

    generate html page from markdown

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -55,7 +55,7 @@ class LeafNode(HTMLNode):
         super().__init__(tag, value, None, props=props)
 
     def to_html(self) -> str:
-        if not self.value:
+        if self.value is None:
             raise ValueError("invalid HTML: no value")
         if not self.tag:
             return self.value
--- src/main.py
+++ src/main.py
@@ -1,7 +1,32 @@
 import os
 import shutil
 
+from block_markdown import markdown_to_html_node
 from copystatic import copystatic
+
+
+def extract_title(markdown: str) -> str:
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    return "Untitled"
+
+def generate_page(from_path: str, template_path: str, dest_path: str):
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+    with open(from_path) as f:
+        contents =  f.read()
+    with open(template_path) as t:
+        template = t.read()
+    html = markdown_to_html_node(contents).to_html()
+    title = extract_title(contents)
+    template = template.replace("{{ Title }}", title)
+    template = template.replace("{{ Content }}", html)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    with open(dest_path, "w") as d:
+        d.write(template)
+
 
 source= "./static"
 destination= "./public"
@@ -11,5 +36,7 @@ def main():
         shutil.rmtree(destination)
 
     copystatic(source, destination)
+
+    generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 main()
